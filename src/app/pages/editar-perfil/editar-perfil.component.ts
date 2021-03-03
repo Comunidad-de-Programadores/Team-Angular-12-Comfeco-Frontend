f1c2ace42df443463c56b304f56e0e7705671f8c
dartcoupli <dartcoupli@hotmail.com>
2021-03-03 17:16:38 +0100

    Edir user #26

--- src/app/pages/editar-perfil/editar-perfil.component.ts
+++ src/app/pages/editar-perfil/editar-perfil.component.ts
@@ -1,17 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { Observable } from 'rxjs';
 import { Country, ListCountries } from 'src/app/models/Country';
 import { EditUserServiceService } from './edit-user-service.service';
-=======
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { CountryService } from 'src/app/services/country.service';
->>>>>>> 5d85f7a49387cea34a14585134350068f606201d
 
 //Podemoms usar jQuery, ahora
-// declare var jQuery:any;
-// declare var $:any;
+declare var jQuery:any;
+declare var $:any;
 
 @Component({
   selector: 'app-editar-perfil',
@@ -30,15 +25,16 @@ export class EditarPerfilComponent implements OnInit {
   {
   }
 
-<<<<<<< HEAD
-  /*loadDates(pais:String)
+   //Load county selected in input from form 
+  public selectedCountry(countrySelected: any):void
   {
-    this.listCountries = this.service.getCountriesCountrys(pais);
-    console.log(this.listCountries);
-  }*/
+    const containerInputCountry = document.getElementById('countryInput') as HTMLInputElement;
+    containerInputCountry.value = countrySelected;
+
+  };
 
   ngOnInit(): void {
-
+   
     //Load input time real
     const countriesInput = document.getElementById('countryInput') as HTMLInputElement;
     
@@ -46,9 +42,17 @@ export class EditarPerfilComponent implements OnInit {
     {
       this.arrayResultFromService = this.servicecountries.getCountriesCountrys(countriesInput.value);
 
-      //console.log("x-> " + this.servicecountries.getCountriesCountrys(countriesInput.value));
+      //Show List Countries
+      var containerListCountriesUl = document.getElementById('listCountriesUl');
+      containerListCountriesUl.style.display = 'block';
     });
 
+    countriesInput.addEventListener("blur", () => 
+    {
+      //No show List Countries
+      var containerListCountriesUl = document.getElementById('listCountriesUl');
+      containerListCountriesUl.style.display = 'none';
+    });
 
     //Load default pictures
     let preview = document.getElementById('preview');
@@ -112,117 +116,5 @@ mostrasPasswordRepeat()
     $('#iconShow').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
   }
 };
-=======
-  file: File;
-  nameFile: string;
-  imagePath: string|ArrayBuffer;
-  listCountries = [];
-
-  formEdit = new FormGroup({
-    nick: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
-    gender: new FormControl('', [Validators.required]),
-    birthday: new FormControl('', [Validators.required]),
-    country: new FormControl('', [Validators.required]),
-    biography: new FormControl('', [Validators.required]),
-    facebook: new FormControl('', [Validators.required]),
-    github: new FormControl('', [Validators.required]),
-    linkedin: new FormControl('', [Validators.required]),
-    twitter: new FormControl('', [Validators.required]),
-  });
-
-  constructor(private countries: CountryService) {
-   }
-
-  ngOnInit(): void {
-
-    this.getCountries();
-    this.initForm();
-
-  //   document.getElementById("file").onchange = function (e:Event) {
-  //   // Creamos el objeto de la clase FileReader
-  //   let reader = new FileReader();
-
-  //   const target = e.target as HTMLInputElement;
-
-  //   // Leemos el archivo subido y se lo pasamos a nuestro fileReader
-  //   reader.readAsDataURL(target.files[0]);
-
-  //   // Le decimos que cuando este listo ejecute el código interno
-  //   reader.onload = function () {
-  //     let preview = document.getElementById('preview');
-
-  //     let img = <HTMLImageElement> document.createElement('img');
-
-  //     img.setAttribute("src", reader.result as string);
-  //     img.setAttribute("style", "border-radius:50%;width:8em;height:8em;margin-top:-125px;");
-
-  //     //preview.innerHTML = '';
-  //     preview.append(img);
-  //   };
-  // }
-}
-
-changeListener($event): void {
-  this.file = $event.target.files[0];
-  console.log(this.file);
-  this.nameFile = this.file['name'];
-  this.readThis($event.target);
-}
-
-readThis(inputValue: any): void {
-  const file: File = inputValue.files[0];
-  const myReader: FileReader = new FileReader();
-
-  myReader.onloadend = (e) => {
-    this.imagePath = myReader.result;
-  };
-
-  myReader.readAsDataURL(file);
-}
-
-async getCountries(){
-  const res: any = await this.countries.getCountries().toPromise();
-  this.listCountries = res;
-  console.log(this.listCountries);
-}
-
-initForm(){
-  this.formEdit.get('nick').setValue('123');
-}
-
-
-
-
-
-
-// mostrarPassword()
-// {
-//   var cambio = document.getElementById("passwordInput") as HTMLInputElement;
-//   if(cambio.type == "password"){
-//     cambio.type = "text";
-//     $('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
-//   }
-//   else
-//   {
-//     cambio.type = "password";
-//     $('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
-//   }
-// };
-
-// mostrasPasswordRepeat()
-// {
-//   var cambio = document.getElementById("passwordRepeatInput") as HTMLInputElement;
-//   if(cambio.type == "password"){
-//     cambio.type = "text";
-//     $('#iconShow').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
-//   }
-//   else
-//   {
-//     cambio.type = "password";
-//     $('#iconShow').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
-//   }
-// };
->>>>>>> 5d85f7a49387cea34a14585134350068f606201d
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,15 +8,12 @@ import { ComponentsModule } from '../components/components.module';
 import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
 import { ProfileComponent } from './profile/profile.component';
 import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
-<<<<<<< HEAD
-import { EditUserServiceService } from './editar-perfil/edit-user-service.service';
-=======
 import { BadgesComponent } from './badges/badges.component';
 import { GroupsComponent } from './groups/groups.component';
 import { EventsComponent } from './events/events.component';
 import { MainProfileComponent } from './main-profile/main-profile.component';
 import { ReactiveFormsModule } from '@angular/forms';
->>>>>>> 5d85f7a49387cea34a14585134350068f606201d
+import { EditUserServiceService } from './editar-perfil/edit-user-service.service';
 
 
 @NgModule({
