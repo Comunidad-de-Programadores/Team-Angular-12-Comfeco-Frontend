968e1cf2e83862edeb27224b9b9e9378fed59b0e
Daniel Pérez <danielzala.v2@gmail.com>
2021-03-10 15:14:27 -0600

    event.component layout

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
+  button;
+  onEvent = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clickJoin(idEvent: number){
+    this.onEvent = true;
+  }
+  
+  clickLeave(idEvent: number){
+    this.onEvent = false;
   }
 
 }
