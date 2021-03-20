3f23dc893cf15423d904998afbf6ebd032071a9b
Luis Gerardo Matehuala Juarez <lg.mj9520@gmail.com>
2021-03-20 03:34:06 -0600

    clean code

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -18,20 +18,6 @@ export class ProfileComponent implements OnInit, OnDestroy {
   userData: any = {};
   socialNetworks;
   badgesUser = [];
-  listMedals = [
-    { name: 'Camper', img: 'assets/img/medals/medal.svg' },
-    { name: 'Lector', img: 'assets/img/medals/medal-2.svg' },
-    { name: 'Primer emoji', img: 'assets/img/medals/medal-3.svg' },
-    {
-      name: 'Preguntas frecuentes leídas',
-      img: 'assets/img/medals/medal-4.svg',
-    },
-    { name: 'Primer enlace', img: 'assets/img/medals/medal-5.svg' },
-    { name: 'Primer me gusta', img: 'assets/img/medals/medal-6.svg' },
-    { name: 'Primera vez que comparte', img: 'assets/img/medals/medal-7.svg' },
-    { name: '¡Bienvenido/a!', img: 'assets/img/medals/medal-8.svg' },
-    { name: 'Buena contribución', img: 'assets/img/medals/medal-9.svg' },
-  ];
 
   config: SwiperOptions = {
     slidesPerView: 4,
